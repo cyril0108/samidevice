c1d26bd3e54777ee09c449feafd83bd2708f3fb5
unknown <>
2016-01-13 17:16:26 +0800

    samidevice

--- samidevice/samidevice.ino
+++ samidevice/samidevice.ino
@@ -1,86 +1,83 @@
+#include <Bridge.h>
 #include <Process.h>
 #include <YunClient.h>
 #include <YunServer.h>
+#include <ArduinoJson.h>
 
 #define PORT 3479
-
 YunClient client;
-YunServer server;
+YunServer server(PORT);
 
 void setup() {
   // Initialize Bridge
   Bridge.begin();
-
   // Initialize Serial
   Serial.begin(9600);
-
-  // Wait until a Serial Monitor is connected.
   while (!Serial);
-
-  // run various example processes
-  runUdp();
+  server.begin();
   runCpuInfo();
 }
 
 void loop() {
-  //tcpConnect();
-  // Make the client connect to the desired server and port
-  //  IPAddress addr(192, 168, 1, 59);
+  YunClient bridgeClient = server.accept();
+  if (bridgeClient) {
+    process(bridgeClient);
+    bridgeClient.stop();
+  }
+  delay (50);
+}
 
-  // Or define it using a single unsigned 32 bit value
-  // IPAddress addr(0xc0a8sab9); // same as 192.168.42.185
+void process(YunClient bridgeClient) {
+  StaticJsonBuffer<1024> jsonBuffer;
+  // read the command
+  String command = bridgeClient.readString();
+  Serial.println(command);
+  Serial.flush();
+  JsonObject& root = jsonBuffer.parseObject(command);
 
-  // Or define it using a byte array
-  // const uint8 addrBytes = {192, 168, 42, 185};
-  // IPAddress addr(addrBytes);
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }
+  String rcvCmd = root["command"];
+  Serial.println("received command : " + rcvCmd);
 
-  //  client.connect(addr, PORT);
-
-  // Or connect by a server name and port.
-  // Note that the Yun doesn't support mDNS by default, so "Yun.local" won't work
-  // client.connect("ServerName.com", PORT);
-
-  // Give some time before trying again
-  //  delay (10000);
+  if (rcvCmd == "queryDisplayInfo") {
+    char *res = "{\"status\" : 200,\"DisplayInfo\" : [\"Temperature\", \"WindStrength\", \"Humidity\"],\"Temperature\": \"30\",\"WindStrength\": \"Medium\",\"Humidity\" : \"60\"}";
+    Serial.println(res);
+    Serial.flush();
+    tcpSendCommand(res);
+  } else if (rcvCmd == "supportCmds") {
+    char *res = "{\"status\":200,\"SupportCmds\":[{\"command\":\"adjustTemperature\",\"command_displayName\":\"Set Temperature\",\"param_type\":\"integer\",\"param_max\":100,\"param_min\":20},{\"command\":\"query\",\"command_displayName\":\"Query Status\",\"param_type\":\"none\"}]";
+    Serial.println(res);
+    Serial.flush();
+    tcpSendCommand(res);
+  }
+  return;
 }
 
 void runUdp() {
-  // Launch "curl" command and get Arduino ascii art logo from the network
-  // curl is command line program for transferring data using different internet protocols
-  Process p;		// Create a process and call it "p"
-  p.begin("python");	// Process that launch the "curl" command
-  p.addParameter("/mnt/sda1/udp.py"); // Add the URL parameter to "curl"
-  p.run();		// Run the process and wait for its termination
-
+  Process p;
+  p.begin("python");
+  p.addParameter("/mnt/sda1/udp.py");
+  p.run();
   Serial.println("UDP runing ....");
   Serial.flush();
 }
 
-void tcpConnect() {
-
+void tcpSendCommand(String sendCmd) {
   // Make the client connect to the desired server and port
-  IPAddress addr(192, 168, 0, 62);
-
-  // Or define it using a single unsigned 32 bit value
-  // IPAddress addr(0xc0a8sab9); // same as 192.168.42.185
-
-  // Or define it using a byte array
-  // const uint8 addrBytes = {192, 168, 42, 185};
-  // IPAddress addr(addrBytes);
-
+  IPAddress addr(192, 168, 1, 59);
   client.connect(addr, PORT);
-
+  //delay (250);
   if (client.connected())
   {
     Serial.println("Connected to the server.");
-
     // Send something to the client
-    client.println("Something...");
-
+    client.println(sendCmd);
     // Cheap way to give the server time to respond.
     // A real application (as opposed to this simple example) will want to be more intelligent about this.
     delay (250);
-
     // Read all incoming bytes available from the server and print them
     while (client.available())
     {
@@ -88,40 +85,6 @@ void tcpConnect() {
       Serial.print(c);
     }
     Serial.flush();
-
-    // Close the connection
-    client.stop();
-  }
-  else
-    Serial.println("Could not connect to the server.");
-  // Or connect by a server name and port.
-  // Note that the Yun doesn't support mDNS by default, so "Yun.local" won't work
-  // client.connect("ServerName.com", PORT);
-
-  // Give some time before trying again
-  delay (1000);
-}
-
-void runTcp() {
-  if (client.connected())
-  {
-    Serial.println("Connected to the server.");
-
-    // Send something to the client
-    client.println("Something...");
-
-    // Cheap way to give the server time to respond.
-    // A real application (as opposed to this simple example) will want to be more intelligent about this.
-    delay (250);
-
-    // Read all incoming bytes available from the server and print them
-    while (client.available())
-    {
-      char c = client.read();
-      Serial.print(c);
-    }
-    Serial.flush();
-
     // Close the connection
     client.stop();
   }
